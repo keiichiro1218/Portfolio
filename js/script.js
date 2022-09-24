7bfe5e5cb1c13ab29852f7b7169df5cd96f76376
千葉桂一郎 <chibakeiichirou@MacBook-Air.local>
2022-09-24 18:12:29 +0900

    レスポンシブ対応

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(function(){
+    
+    $(".openbtn4").click(function () {
+        $(this).toggleClass('active');
+        $(".p-headermenu, body").toggleClass('active');
+    });
+    /* ここにjQueryのコードを書く */
+     
+    });
+    
